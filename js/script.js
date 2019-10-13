a0e8240660da615a204b528027faf688dc0e6075
Pawel Bozemski <paw.boz@gmail.com>
2019-10-13 17:01:19 +0200

    Fix js code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 'use strict';
 
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-});
-
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
